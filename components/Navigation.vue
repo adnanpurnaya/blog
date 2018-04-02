cf897348b083ca8e6a9094a47a424bf938ce1a70
Adnan Purnaya <adnanpurnaya@gmail.com>
2018-04-02 10:55:36 +0700

    back button & share menu

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav>
+  <nav v-resize="initNavigation">
     <v-navigation-drawer
         fixed
         v-model="drawer"
@@ -25,8 +25,9 @@
         </v-list>
       </v-navigation-drawer>
       <v-toolbar color="indigo" dark fixed app>
-        <v-toolbar-side-icon class="hidden-md-and-up" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-toolbar-title>Application</v-toolbar-title>
+        <v-btn v-if="showPost" @click.stop="redirectBack" icon class="hidden-md-and-up"><v-icon color="">arrow_back</v-icon></v-btn>
+        <v-toolbar-side-icon v-else class="hidden-md-and-up" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>{{ appName }}</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
           <v-btn 
@@ -40,19 +41,48 @@
             <v-icon class="mr-1">{{ menu.icon }}</v-icon> {{ menu.text }}
           </v-btn>
         </v-toolbar-items>
+        <ShareMenu class="hidden-md-and-up"/>
       </v-toolbar>
     </nav>
 </template>
 
 <script>
+import ShareMenu from "~/components/ShareMenu";
+
 export default {
+  components: {
+    ShareMenu
+  },
   data: () => ({
+    appName: "",
     drawer: null,
+    showPost: false,
     menus: [
       { text: "Home", link: "/", icon: "home", exact: true },
       // { text: "Post", link: "/post", icon: "library_books", exact: false },
       { text: "About", link: "/about", icon: "contact_mail", exact: true }
     ]
-  })
+  }),
+  methods: {
+    redirectBack() {
+      this.$router.go(-1);
+    },
+    initNavigation(routeName) {
+      routeName = routeName || this.$route.name;
+      this.showPost = routeName == "post-slug";
+      this.appName =
+        routeName == "post-slug" && !this.$vuetify.breakpoint.mdAndUp
+          ? "Post"
+          : "Application";
+    }
+  },
+  mounted() {
+    this.initNavigation();
+  },
+  watch: {
+    $route(to, from) {
+      this.initNavigation(to.name);
+    }
+  }
 };
 </script>
