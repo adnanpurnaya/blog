fe1b35c8643357804bd3b18776ebeadb8221d424
Adnan Purnaya <adnanpurnaya@gmail.com>
2018-04-02 10:56:09 +0700

    load more post

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,37 @@
 <template>
-  <v-layout v-bind="binding" justify-center>
-    <v-flex v-for="post in posts" :key="post.id" md10 offset-md1 lg12 offset-lg0>
+  <v-layout row wrap v-scroll="onScroll">
+    <v-flex v-for="post in posts" :key="post.id" md6 lg4>
       <PreviewPost 
         :img-url="post.imgUrl" 
         :content="post.content" 
         :title="post.title"
         :link="post.slug"
         :published-at="post.publishedAt"
-        />
+      />
     </v-flex>
+    <v-flex style="text-align:center" v-if="loading">
+      <v-progress-circular :size="size" indeterminate color="primary"></v-progress-circular>
+    </v-flex>
+    <v-snackbar bottom color="grey darken-3" v-model="noMoreData">
+      Semua post telah ditampilkan
+      <v-btn flat color="grey lighten-3" @click.native="noMoreData = false">Close</v-btn>
+    </v-snackbar>
   </v-layout>
 </template>
 
 <script>
 import PreviewPost from "~/components/PreviewPost";
 
+const PER_PAGE = 6;
+
 export default {
   components: {
     PreviewPost
   },
+  data: () => ({
+    loading: false,
+    noMoreData: false
+  }),
   asyncData(context) {
     // Check if we are in the editor mode
     let version =
@@ -28,6 +41,8 @@ export default {
     return context.app.$storyapi
       .get("cdn/stories/", {
         starts_with: "post",
+        sort_by: "content.publishedAt:desc",
+        per_page: PER_PAGE,
         version: version
       })
       .then(res => {
@@ -35,30 +50,94 @@ export default {
           posts: res.data.stories.map(post => {
             return {
               id: post.id,
-              slug: post.slug,
+              slug: post.full_slug,
               title: post.content.title,
               imgUrl: post.content.image,
               content: post.content.content,
               publishedAt: post.content.publishedAt
             };
-          })
+          }),
+          total: res.total
         };
       })
       .catch(res => {
-        context.error({
-          statusCode: res.response.status,
-          message: res.response.data
-        });
-        return { posts: [] };
+        if (res.code == "ECONNABORTED") {
+          console.warn(res.message);
+        } else {
+          context.error({
+            statusCode: res.response.status,
+            message: res.response.data
+          });
+        }
+        return { posts: [], total: 0 };
       });
   },
+  methods: {
+    onScroll(event) {
+      const offsetTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      const docHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      if (offsetTop == docHeight) {
+        this.loadMore();
+      }
+    },
+    loadMore() {
+      // Check if we are in the editor mode
+      // let version =
+      // this.$route.query._storyblok ? "draft" : "published";
+      let version = "draft";
+      if (this.loading) {
+        return;
+      }
+      if (this.posts.length >= this.total) {
+        this.noMoreData = true;
+        return;
+      }
+      this.loading = true;
+      this.$nuxt.$loading.start();
+
+      // // Load the JSON from the API
+      return this.$storyapi
+        .get("cdn/stories/", {
+          starts_with: "post",
+          sort_by: "content.publishedAt:desc",
+          per_page: PER_PAGE,
+          page: Math.ceil(this.posts.length / PER_PAGE) + 1,
+          version: version
+        })
+        .then(res => {
+          this.loading = false;
+          this.$nuxt.$loading.finish();
+          res.data.stories.map(post => {
+            let newPost = {
+              id: post.id,
+              slug: post.full_slug,
+              title: post.content.title,
+              imgUrl: post.content.image,
+              content: post.content.content,
+              publishedAt: post.content.publishedAt
+            };
+            this.posts.push(newPost);
+          });
+        })
+        .catch(res => {
+          this.loading = false;
+          this.$nuxt.$loading.finish();
+          if (res.code == "ECONNABORTED") {
+            console.warn(res.message);
+          } else {
+            this.$nuxt.error({
+              statusCode: res.response.status,
+              message: res.response.data
+            });
+          }
+        });
+    }
+  },
   computed: {
-    binding() {
-      const binding = {};
-
-      if (this.$vuetify.breakpoint.mdAndDown) binding.column = true;
-
-      return binding;
+    size() {
+      return this.$vuetify.breakpoint.smAndDown ? 32 : 40;
     }
   }
 };
