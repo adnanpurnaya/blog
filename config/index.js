1f2e5607acd063afea7e18888f2d772780c97d37
Adnan Purnaya <adnanpurnaya@gmail.com>
2018-04-03 11:35:06 +0700

    change app name

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   app: {
-    name: "Adnan Blog",
-    short_name: "Adnan Blog",
+    name: "Adnanp Blog",
+    short_name: "Adnanp Blog",
     description: "A simply readable Web Development Blog app.",
     color: "#00796B"
   },
