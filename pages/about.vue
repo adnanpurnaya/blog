30de3047e32e77d05beed64c2a15de611ef8724f
Adnan Purnaya <adnanpurnaya@gmail.com>
2018-04-03 09:41:59 +0700

    about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-layout row>
+  <v-layout row v-editable="storyblok">
     <v-flex xs12 sm6 offset-sm3>
       <v-card>
-        <v-card-media src="/static/doc-images/lists/ali.png" height="300px">
+        <v-card-media :src="content.image" height="300px">
           <v-layout column class="media">
-            <v-card-title>
+            <!-- <v-card-title>
               <v-btn dark icon>
                 <v-icon>chevron_left</v-icon>
               </v-btn>
@@ -15,25 +15,38 @@
               <v-btn dark icon>
                 <v-icon>more_vert</v-icon>
               </v-btn>
-            </v-card-title>
+            </v-card-title> -->
             <v-spacer></v-spacer>
-            <v-card-title class="white--text pl-5 pt-5">
-              <div class="display-1 pl-5 pt-5">Ali Conners</div>
+            <v-card-title class="white--text pt-5">
+              <div class="display-1 pt-5">{{ content.author }}</div>
             </v-card-title>
           </v-layout>
         </v-card-media>
         <v-list two-line>
-          <v-list-tile v-for="email in emails" 
+          <v-list-tile v-for="email in content.emails" 
             tag="a" target="_top" 
             :key="email.address" 
             :href="'mailto:' + email.address + '?Subject=Hello%20Adnan'"
           >
             <v-list-tile-action>
-              <v-icon color="indigo" v-if="email.icon">{{ email.icon }}</v-icon>
+              <v-icon color="teal lighten-1" v-if="email.icon">{{ email.icon }}</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
               <v-list-tile-title>{{ email.address }}</v-list-tile-title>
               <v-list-tile-sub-title>{{ email.type }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-divider inset></v-divider>
+          <v-list-tile 
+            tag="a" target="_blank" 
+            :href="content.work.website"
+          >
+            <v-list-tile-action>
+              <v-icon color="teal lighten-1">work</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ content.work.name }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ content.work.address }}</v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list>
@@ -51,11 +64,39 @@
 
 <script>
 export default {
-  data: () => ({
-    emails: [
-      { address: "adnanpurnaya.com", type: "Personal", icon: "mail" },
-      { address: "adnan.p@excelindo.co.id", type: "Work" }
-    ]
-  })
+  asyncData(context) {
+    // Check if we are in the editor mode
+    let version =
+      context.query._storyblok || context.isDev ? "draft" : "published";
+
+    // Load the JSON from the API
+    return context.app.$storyapi
+      .get("cdn/stories/about/", {
+        version: version
+      })
+      .then(res => {
+        return {
+          content: JSON.parse(res.data.story.content.body),
+          storyblok: res.data.story.content
+        };
+      })
+      .catch(res => {
+        if (res.code == "ECONNABORTED") {
+          console.warn(res.message);
+        } else {
+          context.error({
+            statusCode: res.response.status,
+            message: res.response.data
+          });
+        }
+        return { content: null, storyblok: null };
+      });
+  },
+  mounted() {
+    this.$storyblok.init();
+    this.$storyblok.on("change", () => {
+      location.reload(true);
+    });
+  }
 };
 </script>
