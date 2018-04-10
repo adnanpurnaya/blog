23e317b8faabb106aa47b4fd99204af3a12aeef8
Adnan Purnaya <adnanpurnaya@gmail.com>
2018-04-10 15:57:18 +0700

    handle ECONNABORTED detail post

--- pages/post/_slug/index.vue
+++ pages/post/_slug/index.vue
@@ -7,7 +7,17 @@
         :title="post.title"
         :link="post.slug"
         :published-at="post.publishedAt"
+        v-if="post"
         />
+      <p style="font-size: 1.5rem" v-else>Post gagal ditampilkan</p>
+      <v-btn flat icon 
+        color="teal darken-1"
+        :loading="loading"
+        @click.native="loadData"
+        :disabled="loading"
+        v-if="!post"
+        ><v-icon>cached</v-icon>
+      </v-btn>
     </v-flex>
   </v-layout>
 </template>
@@ -15,22 +25,24 @@
 <script>
 import Post from "~/components/Post.vue";
 
+// Check if we are in the editor mode
+const VERSION = process.env.NODE_ENV == "development" ? "draft" : "published";
+
 export default {
   components: {
     Post
   },
+  data: () => ({
+    loading: false
+  }),
   asyncData(context) {
-    // Check if we are in the editor mode
-    let version =
-      context.query._storyblok || context.isDev ? "draft" : "published";
-
     // Load the JSON from the API
     return context.app.$storyapi
       .get("cdn/stories/post/" + context.params.slug, {
-        version: version
+        version: VERSION
       })
       .then(res => {
-        var post = res.data.story;
+        const post = res.data.story;
         return {
           post: {
             id: post.id,
@@ -54,6 +66,46 @@ export default {
         }
         return { post: null, storyblok: null };
       });
+  },
+  methods: {
+    loadData() {
+      this.loading = true;
+      this.$nuxt.$loading.start();
+      // Load the JSON from the API
+      this.$storyapi
+        .get("cdn/stories/post/" + context.params.slug, {
+          version: VERSION
+        })
+        .then(res => {
+          this.loading = false;
+          this.$nuxt.$loading.finish();
+
+          const post = res.data.story;
+          this.post = {
+            id: post.id,
+            slug: post.slug,
+            title: post.content.title,
+            imgUrl: post.content.image,
+            content: post.content.content,
+            publishedAt: post.content.publishedAt
+          };
+          this.storyblok = res.data.story.content;
+        })
+        .catch(res => {
+          this.loading = false;
+          this.$nuxt.$loading.finish();
+          if (res.code == "ECONNABORTED") {
+            console.warn(res.message);
+          } else {
+            context.error({
+              statusCode: res.response.status,
+              message: res.response.data
+            });
+          }
+          this.post = null;
+          this.storyblok = null;
+        });
+    }
   },
   mounted() {
     this.$storyblok.init();
